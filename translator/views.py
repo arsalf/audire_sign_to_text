614afa10aaf62d887bbf67d358d3faff73120c63
Arsal Fadilah <88387951+arsalf@users.noreply.github.com>
2022-05-23 19:40:36 +0700

    updatee

--- translator/views.py
+++ translator/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+#domain.com/
 def index(request):
     return render(request, 'index.html')
 
